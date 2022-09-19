1a927a6af21eeed25f5ea76f87994b32c2eb9d04
KushajveerSingh <kushajreal@gmail.com>
2022-09-19 00:39:15 -0400

    change next.config.js to support github pages

--- docs/_next/static/6ddGBw554YLT3GOrMOqfP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/724-e3e668e9796b22b8.js","static/chunks/pages/index-ebf7e8787d052064.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
